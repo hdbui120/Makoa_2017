5f2e8d97c61bc9d57c54c7af2b84313570dd45f9
Hieu Bui <hieubui457@gmail.com>
2019-08-27 15:49:43 -0700

    add descriptions to DriveBase class in subsystem directory

--- src/main/java/org/usfirst/frc/team4619/robot/subsystems/DriveBase.java
+++ src/main/java/org/usfirst/frc/team4619/robot/subsystems/DriveBase.java
@@ -9,21 +9,38 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/*
+	This class create the drive base of the robot
+	The drive base is tank drive style
+	There are 2 type of sensors to control the movemennt of the drive base: encoder and gyroscope
+	encoder for exact distance measurements
+	gyro for angle measurements to control turn
+*/
+
 public class DriveBase extends Subsystem
 {	
+	//initialize 4 motors that drive the robot
 	private VictorSP frontL = new VictorSP(RobotMap.PWM_PORT_3);
 	private VictorSP backL = new VictorSP(RobotMap.PWM_PORT_2);
 	private VictorSP frontR = new VictorSP(RobotMap.PWM_PORT_1);
 	private VictorSP backR = new VictorSP(RobotMap.PWM_PORT_0);
+
+	//declare encoder sensors for both sides to match the rotations
 	private Encoder rightEncoder;
 	private Encoder leftEncoder;
+
+	//declare gyroscope
 	public ADXRS450_Gyro gyro;
+
+	//initialize the P-I-D gains
 	private final static double p = 2;
 	private final static double i = .75;
 	private final static double d = 1;
 	
+	//declare the drive train object
 	public RobotDrive driveTrain;
 	
+	//constructor that initialize all the objects and set the drive train control behavior
 	public DriveBase() {
 		rightEncoder = new Encoder(2, 3, false);
 		leftEncoder = new Encoder(0, 1, true);
@@ -36,14 +53,16 @@ public class DriveBase extends Subsystem
 	    driveTrain = new RobotDrive(frontL, backL, frontR, backR);
 	}
 	
+	//declare the speed of the drive train = 0
 	private double noVector = 0;
 	
-	
+	//method that specify the controlling style of the drive train
 	public void arcadeDrive(double y, double x)
 	{
 		driveTrain.arcadeDrive(y, x, true);
 	}
 	
+	//default command for the drive train enables manual control from joystick
 	@Override
 	protected void initDefaultCommand() 
 	{
@@ -51,16 +70,19 @@ public class DriveBase extends Subsystem
 		setDefaultCommand(new DriveJoystick());
 	} 
 	
+	//method that stop the drive train from doing anything
 	public void doNothing()
 	{
 		frontL.set(noVector);
 	}
 	
+	//move forward at 75%
 	public void moveForward() 
 	{
 		moveForward(0.75);
 	}
 	
+	//move forward at the speed of parameter input
 	public void moveForward(double speed)
 	{
 		frontL.set(speed);
@@ -69,11 +91,13 @@ public class DriveBase extends Subsystem
 		backR.set(-speed);
 	}
 
+	//move back at 75%
 	public void moveBack() 
 	{
 		moveBack(.75);
 	}
 	
+	//move back at the speed of parameter input
 	public void moveBack(double speed)
 	{
 		frontL.set(-speed);
@@ -82,11 +106,14 @@ public class DriveBase extends Subsystem
 		backR.set(speed);
 	}
 	
+	//turn left at 75%
 	public void turnL()
 	{
 		turnL(.75);
 	}
 	
+	//improvisation due to encoder break down
+	//right side needs to rotate slower to match the rotation of the left side
 	public void forwardMod(double speed)
 	{
 		frontL.set(-speed);
@@ -95,6 +122,7 @@ public class DriveBase extends Subsystem
 		backR.set(speed*.965);
 	}
 	
+	//turn left at the speed of parameter input
 	public void turnL(double speed) 
 	{
 		frontL.set(-speed);
@@ -103,11 +131,13 @@ public class DriveBase extends Subsystem
 		backR.set(-speed);
 	}
 
+	//turn right at 75%
 	public void turnR()
 	{
 		turnR(.75);
 	}
 	
+	//turn right at the speed of parameter input
 	public void turnR(double speed) 
 	{
 		frontL.set(speed);
@@ -116,10 +146,12 @@ public class DriveBase extends Subsystem
 		backR.set(speed);
 	}
 
+	//get method that return the right encoder
 	public Encoder getRightEncoder() {
 		return rightEncoder;
 	}
 
+	//get method that return the left encoder
 	public Encoder getLeftEncoder()
 	{
 		return leftEncoder;
